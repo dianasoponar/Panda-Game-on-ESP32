da1ae81cc3905e0d69b3cda56b4a7a9070e07274
Diana Soponar <soponar.diana@gmail.com>
2020-05-22 14:14:58 +0200

    Added sound sensor and a photoresistor

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ def b0_press(pin):
 def b1_press(pin):
     print("Button 1 {}".format("pressed" if pin.value() else game.panda_sleep()))
 
+def sound_rain(pin):
+    print("Sound sensor")
+    game.get_items().make_it_rain()
+
+def light_sleep(pin):
+    if (pin.value() == 1):
+        game.panda_sleep()
 
 b0 = machine.Pin(0, handler=b0_press, trigger=machine.Pin.IRQ_ANYEDGE, debounce=50000, pull=machine.Pin.PULL_UP)
 b1 = machine.Pin(35, handler=b1_press, trigger=machine.Pin.IRQ_ANYEDGE, debounce=50000)
+sound_sensor = machine.Pin(26, handler=sound_rain, trigger=machine.Pin.IRQ_ANYEDGE, debounce=500000)
+sound_sensor = machine.Pin(25, handler=light_sleep, trigger=machine.Pin.IRQ_ANYEDGE, debounce=500000)
 
 framerate = 30
 ms_per_frame = int(1000/24)
@@ -20,7 +29,6 @@ last_time = 0
 hungry_time = ticks_ms()
 sleep_time = ticks_ms()
 panda_blink = 0
-make_rain = ticks_ms()
 while True:
     # tick()   #Run one game step
     tdiff = ms_per_frame - ticks_diff(ticks_ms(), last_time)
@@ -43,19 +51,13 @@ while True:
         hungry_time = ticks_ms()
 
     # drop one hungry bar every 10 seconds
-    if ticks_diff(ticks_ms(), hungry_time) > 10000:
+    if ticks_diff(ticks_ms(), hungry_time) > 40000:
         print(ticks_diff(ticks_ms(), hungry_time))
         game.remove_hungry_segment()
         hungry_time = ticks_ms()
 
     # drop one sleep bar every 10 seconds
-    if ticks_diff(ticks_ms(), sleep_time) > 15000:
+    if ticks_diff(ticks_ms(), sleep_time) > 50000:
         print(ticks_diff(ticks_ms(), sleep_time))
         game.remove_sleep_segment()
         sleep_time = ticks_ms()
-
-    # make it rain
-    if ticks_diff(ticks_ms(), make_rain) > 25000:
-        print("Randomly it starts raining")
-        game.get_items().make_it_rain()
-        make_rain = ticks_ms()
